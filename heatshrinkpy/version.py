903853f85e8b98340264f22e839a349772c61d8c
Marcin Jaworski <marcin@jaworski.me>
2023-06-07 17:35:01 +0200

    Migrate package to Poetry

--- heatshrinkpy/version.py
+++ heatshrinkpy/version.py
@@ -1,1 +1,9 @@
-__version__ = '0.10.0'
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("heatshrinkpy")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
